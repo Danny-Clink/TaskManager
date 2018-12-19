df862e6d7e8563287f67cbd30e83a3da22a19527
Danny_Clink <37295126+Danny-Clink@users.noreply.github.com>
2018-12-19 10:35:15 +0200

    created class for editing projects

--- controllers/managerController.js
+++ controllers/managerController.js
@@ -7,72 +7,73 @@ const connection = mysql.createConnection({
 	database: 'taskmanager'
 });
 
-const Controller = function(){};
+class Projects{
+	async manager(req, res){
+		const projectsList = await this.list();
 
-Controller.manager = async function(req, res){
-	const projectsList = await Controller.projectsList();
+		if(session.role === 'Manager'){
+			res.render('manager',
+				{
+					projectsList: projectsList,
+					userEmail: session.email
+				});
+		}
+	}
 
-	if(session.role === 'Manager'){
-		res.render('manager',
-			{
-				projectsList: projectsList,
-				userEmail: session.email
+	async list(){
+		return new Promise(function(resolve, reject){
+			connection.query('SELECT * FROM projects WHERE manager = ?',
+				[session.email], (err, result) => {
+					if (err) throw reject;
+
+					resolve(result);
+				});
+		});
+	}
+
+	create(req, res){
+		const name = req.body.createName;
+		const description = req.body.createDescription;
+
+		connection.query('SELECT true FROM projects WHERE name = ? LIMIT 1',
+			[name], (err, result) => {
+				if (err) throw err;
+
+				if (result.length === 0){
+					connection.query('INSERT INTO projects (name, description, manager) VALUE(?, ?, ?)',
+						[name, description, session.email], (err) => {
+							if (err) throw err;
+
+							res.redirect('/manager');
+						});
+				} else res.send(404, '<h1>This project already exist</h1>');
 			});
-	} else res.send('access danied');
-};
+	}
 
-Controller.projectsList = function(){
-	return new Promise(function(resolve, reject){
-		connection.query('SELECT * FROM projects WHERE manager = ?',
-			[session.email], (err, result) => {
-				if (err) throw reject;
+	update(req, res){
+		const updateName = req.body.updateSelectName;
+		const name = req.body.updateName;
+		const description = req.body.updateDescription;
 
-				resolve(result);
+		connection.query('UPDATE projects SET name = ?, description = ? WHERE name = ?',
+			[name, description, updateName], (err) => {
+				if(err) throw err;
+
+				res.redirect('/manager');
 			});
-	});
-};
+	}
 
-Controller.projectCreate = function(req, res){
-	const name = req.body.nameCreate;
-	const description = req.body.descriptionCreate;
+	delete(req, res){
+		const name = req.body.deleteName;
 
-	connection.query('SELECT true FROM projects WHERE name = ? LIMIT 1',
-		[name], (err, result) => {
-			if (err) throw err;
+		connection.query('DELETE FROM projects WHERE name = ? LIMIT 1',
+			[name], (err) => {
+				if (err) throw err;
 
-			if (result.length === 0){
-				connection.query('INSERT INTO projects (name, description, manager) VALUE(?, ?, ?)',
-					[name, description, session.email], (err) => {
-						if (err) throw err;
+				res.redirect('/manager');
+			});
+	}
+}
 
-						res.redirect('/manager');
-					});
-			} else res.send(404, '<h1>This project already exist</h1>');
-		});
-};
 
-Controller.projectUpdate = function(req, res){
-	const id = req.body.idUpdate.replace(/[^0-9]/gim, '');
-	const name = req.body.nameUpdate;
-	const description = req.body.descriptionUpdate;
-
-	connection.query('UPDATE projects SET name = ?, description = ? WHERE ID = ?',
-		[name, description, id], (err) => {
-			if(err) throw err;
-
-			res.redirect('/manager');
-		});
-};
-
-Controller.projectDelete = function(req, res){
-	const id = req.body.idDelete.replace(/[^0-9]/gim, '');
-
-	connection.query('DELETE FROM projects WHERE ID = ? LIMIT 1',
-		[id], (err) => {
-			if (err) throw err;
-
-			res.redirect('/manager');
-		});
-};
-
-module.exports = Controller;
+module.exports = Projects;
--- routes/manager.js
+++ routes/manager.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const checkAuth = require('../middleware/checkAuth');
 
 const managerController = require('../controllers/managerController');
+const projects = new managerController();
 
-router.get('/', checkAuth,  managerController.manager);
-router.post('/create', checkAuth, managerController.projectCreate);
-router.post('/update', checkAuth, managerController.projectUpdate);
-router.post('/delete', checkAuth, managerController.projectDelete);
+router.get('/', checkAuth, (req, res) => {return projects.manager(req, res);});
+router.post('/create', checkAuth, (req, res) => {return projects.create(req, res);});
+router.post('/update', checkAuth, (req, res) => {return projects.update(req, res);});
+router.post('/delete', checkAuth, (req, res) => {return projects.delete(req, res);});
 
 module.exports = router;
